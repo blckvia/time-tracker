75e3fcc7e0b028ee53599f9616bca35c7ca16b01
blckvia <a.vyatkinn@icloud.com>
2024-07-05 13:19:19 +0300

    fix(docs): regenerate link

--- docs/docs.go
+++ docs/docs.go
@@ -680,7 +680,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "127.0.10.5:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Time Tracker API",
